1e4cb87f0a96d995a47fb46d97c8a1a97c691fdc
oated <nathapott@gmail.com>
2024-02-26 22:42:59 +0700

    db fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,9 +5,9 @@
           connection: {
             host: env('DATABASE_HOST', 'localhost'),
             port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'bank'),
-            user: env('DATABASE_USERNAME', 'postgres'),
-            password: env('DATABASE_PASSWORD', '0000'),
+            database: env('DATABASE_NAME', 'cookai'),
+            user: env('DATABASE_USERNAME', 'cookai'),
+            password: env('DATABASE_PASSWORD', 'cookai-ppwd'),
             schema: env('DATABASE_SCHEMA', 'public'), // Not required
             ssl: {
               cd: env('DATABASE_CA')
